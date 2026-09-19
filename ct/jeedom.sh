0dd622822bb5104f192667dadefa9ee275be9caa
Salvialf <jeuxalf@live.fr>
2026-09-19 22:39:50 +0200

    feat(jeedom): add initial Jeedom LXC and install scripts

---
+++ ct/jeedom.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/Salvialf/pve-scripts/main/lib/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Mips2648
+# Modified by: Salvialf
+# License: MIT
+# https://github.com/Salvialf/pve-scripts/raw/main/LICENSE
+# Source: https://jeedom.com/
+
+function header_info {
+clear
+cat <<"EOF"
+       __              __
+      / /__  ___  ____/ /___  ____ ___
+ __  / / _ \/ _ \/ __  / __ \/ __ `__ \
+/ /_/ /  __/  __/ /_/ / /_/ / / / / / /
+\____/\___/\___/\__,_/\____/_/ /_/ /_/
+
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="Jeedom"
+var_disk="16"
+var_cpu="2"
+var_ram="2048"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+header_info
+if [[ ! -f /var/www/html/core/config/version ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+msg_info "Updating $APP LXC OS packages"
+apt-get update &>/dev/null
+apt-get -y upgrade &>/dev/null
+msg_ok "Updated $APP LXC OS packages"
+echo -e "You can now update Jeedom itself from its Web UI."
+exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
---
+++ install/jeedom-install.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Mips2648
+# Modified by: Salvialf
+# License: MIT
+# https://github.com/Salvialf/pve-scripts/raw/main/LICENSE
+# Source: https://jeedom.com/
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+  lsb-release \
+  git
+msg_ok "Installed Dependencies"
+
+DEFAULT_BRANCH="master"
+REPO_URL="https://github.com/jeedom/core.git"
+
+echo
+while true; do
+  read -rp "Enter Jeedom branch to install (master, release, develop...) (Default: ${DEFAULT_BRANCH}): " BRANCH
+  BRANCH="${BRANCH:-$DEFAULT_BRANCH}"
+
+  if git ls-remote --heads "$REPO_URL" "refs/heads/$BRANCH" | grep -q .; then
+    break
+  else
+    msg_error "Branch '$BRANCH' does not exist on remote. Please try again."
+  fi
+done
+
+msg_info "Downloading Jeedom installation script"
+cd /tmp
+wget -q https://raw.githubusercontent.com/jeedom/core/"${BRANCH}"/install/install.sh
+chmod +x install.sh
+msg_ok "Installation script downloaded"
+
+msg_info "Installing Jeedom main dependencies, please wait"
+$STD ./install.sh -v "$BRANCH" -s 2
+msg_ok "Installed Jeedom main dependencies"
+
+msg_info "Installing database"
+$STD ./install.sh -v "$BRANCH" -s 3
+msg_ok "Installed database"
+
+msg_info "Installing Apache"
+$STD ./install.sh -v "$BRANCH" -s 4
+msg_ok "Installed Apache"
+
+msg_info "Installing PHP and dependencies"
+$STD ./install.sh -v "$BRANCH" -s 5
+msg_ok "Installed PHP"
+
+msg_info "Downloading Jeedom core"
+$STD ./install.sh -v "$BRANCH" -s 6
+msg_ok "Downloaded Jeedom core"
+
+msg_info "Customizing database"
+$STD ./install.sh -v "$BRANCH" -s 7
+msg_ok "Customized database"
+
+msg_info "Customizing Jeedom"
+$STD ./install.sh -v "$BRANCH" -s 8
+msg_ok "Customized Jeedom"
+
+msg_info "Configuring Jeedom"
+$STD ./install.sh -v "$BRANCH" -s 9
+msg_ok "Configured Jeedom"
+
+msg_info "Installing Jeedom"
+$STD ./install.sh -v "$BRANCH" -s 10
+msg_ok "Installed Jeedom"
+
+msg_info "Running post-installation steps"
+$STD ./install.sh -v "$BRANCH" -s 11
+msg_ok "Post-installation done"
+
+msg_info "Checking installation"
+$STD ./install.sh -v "$BRANCH" -s 12
+msg_ok "Installation checked, a reboot is recommended"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -f /tmp/install.sh
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
