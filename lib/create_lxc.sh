4c891531f0bf2b189bcf4d0ccee93b93d4aaed25
Salvialf <jeuxalf@live.fr>
2026-09-26 10:50:53 +0200

    fix(error-handler): capture exit code before it gets overwritten

--- lib/create_lxc.sh
+++ lib/create_lxc.sh
@@ -26,9 +26,9 @@ trap 'error_handler $LINENO "$BASH_COMMAND"' ERR
 
 # This function handles errors
 function error_handler() {
+  local exit_code="$?"
   if [ -n "$SPINNER_PID" ] && ps -p $SPINNER_PID > /dev/null; then kill $SPINNER_PID > /dev/null; fi
   printf "\e[?25h"
-  local exit_code="$?"
   local line_number="$1"
   local command="$2"
   local error_message="${RD}[ERROR]${CL} in line ${RD}$line_number${CL}: exit code ${RD}$exit_code${CL}: while executing command ${YW}$command${CL}"
